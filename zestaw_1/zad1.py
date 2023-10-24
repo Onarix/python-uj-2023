54d45e1844ef98f6d5a4e0344c51d89e95dc205b
Onarix <84595060+Onarix@users.noreply.github.com>
2023-10-24 23:53:24 +0200

    update zestaw_1

--- zestaw_1/zad1.py
+++ zestaw_1/zad1.py
@@ -7,25 +7,25 @@ for i in range(1, len(argv)):
     coefs = ""
     div = 2
     num = int(float(sys.argv[i]))
-    while (num != 1):
-        if ((num % div) == 0):
-            num = num/div
+    while num != 1:
+        if (num % div) == 0:
+            num = num / div
             coef.append(div)
         else:
             div = div + 1
-    
-    while(len(coef) != 0):
+
+    while len(coef) != 0:
         for x in coef:
             k = len([i for i, n in enumerate(coef) if n == x])
-            while (coef.count(x)):
+            while coef.count(x):
                 coef.remove(x)
             if not coefs:
-                if (k == 1):
+                if k == 1:
                     coefs = str(x)
                 else:
                     coefs = str(x) + "^" + str(k)
             else:
-                if (k == 1):
+                if k == 1:
                     coefs = coefs + "*" + str(x)
                 else:
                     coefs = coefs + "*" + str(x) + "^" + str(k)
--- zestaw_1/zad2.py
+++ zestaw_1/zad2.py
@@ -1,5 +1,5 @@
-'''|....|....|....|....|....|....|....|....|....|....|....|....|
-   0    1    2    3    4    5    6    7    8    9   10   11   12'''
+"""|....|....|....|....|....|....|....|....|....|....|....|....|
+   0    1    2    3    4    5    6    7    8    9   10   11   12"""
 
 length = int(input("Input length: "))
 meter = "|"
@@ -7,8 +7,10 @@ numbers = "0"
 
 for i in range(length):
     meter = meter + "....|"
-    #TODO: Ogarnąć liczby przy miarce - tak żeby ostatnia cyfra wielocyfrowej liczby była przy |
-    numbers = numbers + ' '*(5-len(str(i))) + str(i+1)
+    if len(str(i)) < len(str(i + 1)):
+        numbers = numbers + " " * (5 - (len(str(i + 1)))) + str(i + 1)
+    else:
+        numbers = numbers + " " * (5 - (len(str(i)))) + str(i + 1)
 
 print(meter)
 print(numbers)
